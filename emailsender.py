41a3b5e0a778ad55bd24c389fd06966e4b81d9d3
NITESH PRAJAPAT <69027822+niteshprajapat@users.noreply.github.com>
2020-08-05 12:09:52 -0700

    Create emailsender.py

---
+++ emailsender.py
@@ -0,0 +1,16 @@
+import smtplib
+
+to=input("enter receiver email")
+content=input("enter message for email")
+sender=input("enter sender email")
+pwd=input("enter password")
+
+def email(to,content):
+    server=smtplib.SMTP("smtp.gmail.com",587)
+    server.ehlo()
+    server.starttls()
+    server.login(sender,pwd)
+    server.sendmail(sender,to,content)
+    server.close()
+
+email(to,content)
